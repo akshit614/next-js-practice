d862b82f151fbc58c475a00c7ef8c95eea12cdc7
akshit614 <akkisingh8266@gmail.com>
2025-02-27 22:55:50 +0530

    Async component in next

---
+++ backend-with-next/app/page.tsx
@@ -0,0 +1,21 @@
+import axios from "axios";
+// Async components is a feature of next js to fecth details from server to take advantage of SSR, can not be used in client component
+
+
+const getData = async () => {
+  const res = await axios.get('https://jsonplaceholder.typicode.com/posts');
+  return res.data;
+}
+
+
+// Async component
+export default async function Home() {
+  const data = await getData();
+  return (
+    <>
+      <div>Hi there...</div>
+      <div>Id --- {JSON.stringify(data[0].id)}</div>
+      <div>Title --- {JSON.stringify(data[0].title)}</div>
+    </>
+  );
+}
