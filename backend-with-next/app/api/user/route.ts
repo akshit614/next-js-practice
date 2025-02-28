1582e44402f2e78a6418296d854794a2f988a5df
akshit614 <akkisingh8266@gmail.com>
2025-02-28 22:50:06 +0530

    Learning backend and post routes in next

--- backend-with-next/app/api/user/route.ts
+++ backend-with-next/app/api/user/route.ts
@@ -1,8 +1,24 @@
 // file for backend route localhost:3000/api/user
+// in next you have to specify api foler for backend creation and should have to name file as route.ts
+// like page.tsx for frontend 
+// Yes it's opinionated 
+
+import { NextRequest } from "next/server";
+
 
 export function GET() {
     return Response.json({
         username : "akki",
         password: "akki@123"
+    })
+}
+
+export async function POST(req : NextRequest) {
+    const body = await req.json();
+    console.log(body);
+    
+
+    return Response.json({ 
+        message : "logged in"
     })
 }
---
+++ backend-with-next/app/components/Signup.tsx
@@ -0,0 +1,42 @@
+"use client"
+
+import axios from "axios";
+import { useState } from "react"
+
+export default function Signup() {
+
+const [username, setUsername] = useState("");
+const [password, setPassword] = useState("");
+
+    return (
+        <div className="flex flex-col justify-center h-screen ">
+            <div className="flex  justify-center">
+                <div className="max-w-sm border shadow-lg rounded-xl p-2 m-4">
+
+                <p className="p-4 font-extrabold text-3xl text-center">Signup</p>
+                <div className="flex flex-col p-2">
+                    <label className="font-bold" htmlFor="">Username</label>
+                    <input  onChange={(e) => {
+                        setUsername(e.target.value)
+                    }}   className="border rounded-md px-1" type="text"  placeholder="Username"/>
+                </div>
+                <div className="flex flex-col p-2">
+                    <label className="font-bold" htmlFor="">Password</label>
+                    <input  onChange={(e) => {
+                        setPassword(e.target.value)
+                    }}   className="border rounded-md px-1" type="password"  placeholder="assword"/>
+                </div>
+
+                <div className="pt-4 pb-4 text-center">
+                    <button onClick={async () => {
+                        await axios.post("http://localhost:3000/api/user", {
+                            username,
+                            password
+                        })
+                    }} className="border-4 p-1 px-5 rounded-full font-bold hover:bg-black hover:text-white hover:border-black transition-all">SignUp</button>
+                </div>
+                </div>
+            </div>
+        </div>
+    )
+}
