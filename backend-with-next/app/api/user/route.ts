6023b8880d20397b1d603431d522455a3f4e7e66
akshit614 <akkisingh8266@gmail.com>
2025-03-01 23:36:43 +0530

    connected DB and got dynamic responses

--- backend-with-next/app/api/user/route.ts
+++ backend-with-next/app/api/user/route.ts
@@ -3,19 +3,31 @@
 // like page.tsx for frontend 
 // Yes it's opinionated 
 
+import { PrismaClient } from "@prisma/client";
 import { NextRequest, NextResponse } from "next/server";
 
+const client = new PrismaClient();
 
-export function GET() {
+export async function GET() {
+    const user = await client.user.findFirst({})
     return Response.json({
-        username : "akki",
-        password: "akki@123"
+        username : user?.username,
+        password: user?.password
     })
 }
 
 export async function POST(req : NextRequest) {
     const body = await req.json();
     console.log(body);
+    const user = await client.user.create({
+        data : {
+            username : body.username,
+            password : body.password
+        }
+
+    })
+
+    console.log(user.id);
     
 
     return NextResponse.json({ 
