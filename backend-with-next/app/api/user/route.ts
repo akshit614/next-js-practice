971f88631baf34b4f90f326fb03d2806f5e8155d
akshit614 <akkisingh8266@gmail.com>
2025-03-02 23:24:36 +0530

    updated backend for server actions

--- backend-with-next/app/api/user/route.ts
+++ backend-with-next/app/api/user/route.ts
@@ -3,6 +3,7 @@
 // like page.tsx for frontend 
 // Yes it's opinionated 
 
+import { signup } from "@/app/actions/user";
 import client from "@/db"
 import { NextRequest, NextResponse } from "next/server";
 
@@ -18,26 +19,42 @@ export async function POST(req : NextRequest) {
     const body = await req.json();
     console.log(body);
 
-    try {
-        const user = await client.user.create({
-            data : {
-                username : body.username,
-                password : body.password
-            }
-    
-        })
+    // bcz of server actions
 
+    const res =await signup(body.username, body.password);
+
+    if (res) {
         return NextResponse.json({ 
             message : "logged in"
         })
-    
-        console.log(user.id);
-    } catch (error) {
-        console.log(error);
+    }
+    else {
         return NextResponse.json({
             message : "Error while signing up"
         })
     }
+
+
+    // try {
+    //     const user = await client.user.create({
+    //         data : {
+    //             username : body.username,
+    //             password : body.password
+    //         }
+    
+    //     })
+
+    //     return NextResponse.json({ 
+    //         message : "logged in"
+    //     })
+    
+    //     console.log(user.id);
+    // } catch (error) {
+    //     console.log(error);
+    //     return NextResponse.json({
+    //         message : "Error while signing up"
+    //     })
+    // }
 
     
     
